0800306dc456307e56d360c6899badf237abbbbc
WayneSan <waynesan@zerozone.tw>
2014-07-16 01:23:27 +0800

    Added a directive `scrollBottomOnMessage` to scroll to bottom on `$scope.messages` changing.

--- assets/linker/javascripts/chatroom.js
+++ assets/linker/javascripts/chatroom.js
@@ -7,3 +7,15 @@
  */
 var app = angular.module('chatroom', ['ui.bootstrap']);
 
+app.directive('scrollBottomOnMessage', function() {
+  return function(scope, element, attrs) {
+    scope.$watch(function() { return scope.messages }, function(messages) {
+      //console.log('scrollBottomOnMessage', messages);
+      if (messages && messages.length) {
+        $(element).animate({
+          scrollTop: element.prop('scrollHeight')
+        }, 1000);
+      }
+    });
+  };
+});
