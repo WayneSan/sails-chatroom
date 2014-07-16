62e09a7b6ed695775328ecc7c7def85e3c897474
WayneSan <waynesan@zerozone.tw>
2014-07-17 00:12:47 +0800

    Added `sendMessage()` function and on message event implementation.

--- assets/linker/javascripts/controllers/MessageController.js
+++ assets/linker/javascripts/controllers/MessageController.js
@@ -13,4 +13,23 @@ app.controller('MessageController', function($scope, SailsSocket, $log) {
         $scope.messages = response.reverse();
       });
   });
+  
+  $scope.$on('SailsSocket:message', function(ev, data) {
+    $log.debug('SailsSocket :: $on(message)', data);
+    if (data.model === 'message') {
+      switch(data.verb) {
+        case 'create':
+          $scope.messages.push(data.data);
+          break;
+      }
+    }
+  });
+
+  $scope.sendMessage = function() {
+    $log.debug('sendMessage :: myMessage =', $scope.myMessage);
+    if ($scope.myMessage && $scope.myMessage.text.trim() !== "") {
+      SailsSocket.post('/message', $scope.myMessage);
+      $scope.myMessage.text = "";
+    }
+  };
 });
